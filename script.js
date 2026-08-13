588feebfc13b5f7d39867186423b6ea991b12252
silvasantosgabriel0702-lgtm <silva.santos.gabriel0702@escola.pr.gov.br>
2026-08-12 21:51:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const botoes = document.querySelectorAll("button");
+
+botoes.forEach(function (botao) {
+    let curtiu = false;
+    botao.addEventListener("click", botaoClicado);
+
+    function botaoClicado() {
+        console.log("fui clicado");
+        let texto = botao.querySelector("span");
+
+        if (curtiu === false) {
+            texto.textContent++;
+            curtiu = true;
+        } else {
+            texto.textContent--;
+            curtiu = false;
+        }
+    }
+});
+const botaoTema = document.querySelector('.btn-tema-escuro');
+
+botaoTema.addEventListener('click', () => {
+    document.body.classList.toggle('tema-escuro');
+});
+
+function mudaTema() {
+  const corpoPagina = document.body;
+  if (corpoPagina.classList.contains("tema-escuro")) {
+    corpoPagina.classList.remove("tema-escuro");
+  } else {
+    corpoPagina.classList.add("tema-escuro");
+  }
+}
